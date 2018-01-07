63fa1ede56d1f42d702a7f5b8c8ab005fb5b4d39
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-07 12:36:36 -0600

    outputs proper JSON

--- csv-to-json.js
+++ csv-to-json.js
@@ -6,15 +6,26 @@ const uuidv1 = require('uuid/v1');
 convert(process.argv[2]);
 
 function convert(csvFilePath){
+  let buffer = [];
   csvtojson().fromFile(csvFilePath)
        .on('json', jsonObj => {
-         output(jsonObj);
+        //  console.log(jsonObj);
+        buffer.push(jsonObj);
        })
        .on('done', error => {
-         if(error) console.error(error);
+         if(error) {
+           console.error(error);
+           return
+          };
+         output(buffer);
+         console.log(buffer);
        });
 }
 
-function output(jsonObj){
-  console.log(jsonObj)
+function output(jsonArry){
+  const folderName = uuidv1();
+  fs.mkdirSync(folderName);
+  fs.writeFileSync(path.join(__dirname, folderName, 'jsonFromCsv.json'), JSON.stringify(jsonArry, null, 2))
+  console.log(jsonArry)
+  console.log("finished");
 }
